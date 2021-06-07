b0b12df90c5c5f7d9faade5c7d2876d1d571d8ed
Nomercy-ops <chauhanrikesh786@gmail.com>
2021-06-07 17:15:07 +0530

    Added : services folder with addressbook.java file and  display welcome message

---
+++ services/AddressBook.java
@@ -0,0 +1,20 @@
+/**
+ *
+ * @author Rikesh Chhetri
+ * @version 1.0
+ * @Created_on: 07.06.21
+ *
+ * purpose: Managing user address book,user can add delete edit and show person
+ * details.
+ *
+ */
+
+package com.bridgelabz.AddressBook.services;
+
+public class AddressBook {
+	
+	public static void main(String[] args) {
+      
+        System.out.println("Welcome to Address Book Program");
+}
+}
