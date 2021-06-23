7b9d145182563c3a5ec1991d596b3a52edfa48d6
Nomercy-ops <chauhanrikesh786@gmail.com>
2021-06-23 19:19:07 +0530

    Added: boolean equals check method inside person info model file and ith check new person firstname and old personlist firstname for overiding of duplicates values

--- model/PersonInfo.java
+++ model/PersonInfo.java
@@ -1,4 +1,6 @@
-package com.bridgelabz.AddressBook.Model;
+package com.bridgelabz.addressbook.Model;
+
+import java.util.Objects;
 
 /**
  * this is PersonInfo class model for address book here getter and setter is
@@ -8,14 +10,14 @@ package com.bridgelabz.AddressBook.Model;
  */
 public class PersonInfo {
 
-    public String firstName;
+    private String firstName;
     private String lastName;
     private String address;
     private String city;
     private String state;
     private String email;
-    private int zipcode;
-    private long phoneNumber;
+    private String zipcode;
+    private String phoneNumber;
 
     /*The getter method returns the value of the attribute.
     The setter method takes a parameter and assigns it to the attribute.*/
@@ -67,19 +69,19 @@ public class PersonInfo {
         this.email = email;
     }
 
-    public int getZipcode() {
+    public String getZipcode() {
         return zipcode;
     }
 
-    public void setZipcode(int zipcode) {
+    public void setZipcode(String zipcode) {
         this.zipcode = zipcode;
     }
 
-    public long getPhoneNumber() {
+    public String getPhoneNumber() {
         return phoneNumber;
     }
 
-    public void setPhoneNumber(long phoneNumber) {
+    public void setPhoneNumber(String phoneNumber) {
         this.phoneNumber = phoneNumber;
     }
 
@@ -88,5 +90,43 @@ public class PersonInfo {
         return "PersonInfo{" + "firstName=" + firstName + ", lastName=" + lastName + ", address=" + address + ", city=" + city + ", state=" + state + ", email=" + email + ", zipcode=" + zipcode + ", phoneNumber=" + phoneNumber + '}';
     }
 
-    //This method returns the string itself.
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 29 * hash + Objects.hashCode(this.firstName);
+        hash = 29 * hash + Objects.hashCode(this.lastName);
+        return hash;
+    }
+
+    /**
+     * uc7: Ability to ensure there is no Duplicate Entry of the same Person in
+     * a particular Address.
+     * 
+     * This method checks if some other Object passed
+     * to it as an argument is equal to the Object on which it is invoked.
+     *
+     * @param obj
+     * @return
+     */
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final PersonInfo other = (PersonInfo) obj;
+        if (!Objects.equals(this.firstName, other.firstName)) {
+            return false;
+        }
+        if (!Objects.equals(this.lastName, other.lastName)) {
+            return false;
+        }
+        return true;
+    }
+
 }
---
+++ services/AddressBookSystem.java
@@ -0,0 +1,171 @@
+/**
+ *
+ * @author Rikesh Chhetri
+ * @version 1.0
+ * @Created_on: 07.06.21
+ *
+ * purpose: Managing user address book,user can add delete edit and show person
+ * details.
+ *
+ */
+
+package com.bridgelabz.addressbook.services;
+
+import com.bridgelabz.AddressBook.Model.PersonInfo; // importing person or user model from model package.
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.Set;
+
+public class AddressBookSystem {
+
+    //Storing person info list inside address book map and then storing it to hashmap. 
+    private static Map<String, Set<PersonInfo>> addressBookMap = new HashMap();
+    private static Set<PersonInfo> personList = new HashSet<>();
+
+    private String addressBookName;
+    private static final Scanner scanner = new Scanner(System.in);
+
+    /**
+     * This is the main method from here welcome message is display.
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        // creating object of a addressbook class
+
+        AddressBookSystem addressbook = new AddressBookSystem();
+        System.out.println("Welcome to Address Book Program");
+
+        boolean isExit = false;
+        while (!isExit) {
+
+            System.out.println("Enter options\n1.Add AddressBook\n2.Edit\n3.Delete\n4.show contacts\n5.search\n6.Exit");
+            int userInput = scanner.nextInt();
+            switch (userInput) {
+                case 1:
+
+                    addressbook.addAddressBook();
+
+                    break;
+                case 2:
+                    addressbook.editUser();
+                    break;
+                case 3:
+                    addressbook.deleteUser();
+                    break;
+                case 4:
+                    addressbook.showContacts();
+                    break;
+                case 5:
+                    UserInputData.search();
+
+                    break;
+                case 6:
+                    isExit = true;
+                    break;
+                default:
+                    System.out.println("Please enter valid input");
+            }
+        } // end of while loop
+    }
+
+    /**
+     * uc2- getting input from user and adding it to the address book. for
+     * getting personlist with details of person like firstName etc,
+     * UserInputData class is created and inside addUserInfo it personList is
+     * returned,
+     *
+     * @see line 85 putting that returned value into personlist.and finally
+     * putting it into address book map.
+     */
+    private void addAddressBook() {
+
+        Set<PersonInfo> personList1 = new HashSet<>();
+        System.out.println("Enter the name for your AddressBook : ");
+        addressBookName = scanner.next();
+
+        // checking if addressbookmap contain the userinput  addressbook.
+        if (addressBookMap.containsKey(addressBookName)) {
+            System.out.println(addressBookName + " is already there please choose another name. ");
+            addAddressBook();
+        } else {
+
+            UserInputData userDetails = new UserInputData();
+            personList1 = userDetails.addUserInfo();
+
+            // checking for duplicate values if exist or not.
+            if (!(personList1.equals(personList))) {
+                addressBookMap.put(addressBookName, personList1);
+                System.out.println("person added successfully");
+
+            }
+        }
+    }
+
+    /**
+     * uc3 - getting name from user and then checking in the personlist. getting
+     * user with the username and then comparing with it. if user enter name is
+     * true then only the user can edit the data.
+     *
+     */
+    public void editUser() {
+
+        System.out.println("Enter the first name to edit contact.");
+        String contactName = scanner.next();
+
+        addressBookMap.entrySet().stream().map(entry -> entry.getValue().iterator()).forEachOrdered(itr -> {
+            while (itr.hasNext()) {
+                if (itr.next().getFirstName().equals(contactName)) {
+                    UserInputData userDetails = new UserInputData();
+                    personList = userDetails.addUserInfo();
+                    System.out.println("Contact edited with given first name : " + contactName);
+                    addressBookMap.put(addressBookName, (Set<PersonInfo>) personList);
+
+                } else {
+                    System.out.println("Person with this name not found please try with another name.");
+                    editUser();
+                }
+            }
+        });
+
+    }
+
+    /**
+     * uc4- getting first name from user and then validating with the personlist
+     * name. user will be able to delete the data or record if the inserted
+     * input is true.
+     *
+     */
+    public void deleteUser() {
+
+        System.out.println("Enter the first name : ");
+        String userInput = scanner.next();
+
+        addressBookMap.entrySet().stream().map(entry -> entry.getValue().iterator()).forEachOrdered(itr -> {
+            while (itr.hasNext()) {
+                if (itr.next().getFirstName().equals(userInput)) {
+                    itr.remove();
+                    System.out.println("Contact deleted successfully");
+                }
+
+            }
+        });
+
+    }
+
+    /**
+     * Displaying user details from the address book map.
+     * it will show all the name of address books and person lists inside that address book.
+     */
+    private void showContacts() {
+        addressBookMap.forEach((String key, Set<PersonInfo> value) -> {
+            System.out.println(
+                    "AddressBook Name : " + key + "\t\t"
+                    + "Person Details : " + value);
+        });
+    }
+
+}
